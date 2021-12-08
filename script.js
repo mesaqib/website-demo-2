b2038c46ecda37be70ea82a2ccbf69edc1131594
Saqib <57936287+mesaqib@users.noreply.github.com>
2021-12-09 02:26:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var toggleOpen = document.querySelector(".toggle");
+var mobileMenu = document.querySelector(".mobile__menu");
+
+var open = document.querySelector(".open");
+
+
+
+toggleOpen.addEventListener("click", function(){
+     
+    document.querySelector(".toggle");
+
+    toggleOpen.classList.toggle("open");
+     mobileMenu.classList.toggle("open");
+
+})
+
+
+
+
+
+// Animation 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
